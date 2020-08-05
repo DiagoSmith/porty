c6c070be7f0c453f557cf63a3ae31343d47eef55
Roberto Floris <rmfloris@gmail.com>
2020-08-05 23:14:41 +0200

    chore: remove ts eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
     rules: {
         'react/react-in-jsx-scope': 'off',
         'react/prop-types': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     },
